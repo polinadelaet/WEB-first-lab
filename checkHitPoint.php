865081997b515ac0b78a334ec9ec94caab5abb5b
Lina <polina.usui@gmail.com>
2020-09-22 00:33:35 +0300

    final

--- checkHitPoint.php
+++ checkHitPoint.php
@@ -1,5 +1,80 @@
 <?php
-    if (isset($_POST['coordinateX'],$_POST['coordinateY'], $_POST['coordinateZ'])) {
-        print_r($_POST);
-    }  //print_r($_POST);
+    $start = microtime(true);
+    date_default_timezone_set('Europe/Moscow');
+    const Y_VALUES = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
+    const R_VALUES = [1, 2, 3, 4, 5];
+
+    if (isset($_POST['coordinateX'],$_POST['coordinateY'], $_POST['coordinateR'])) {
+
+        $x = $_POST['coordinateX'];
+        $y = $_POST['coordinateY'];
+        $r = $_POST['coordinateR'];
+
+        if (isValid($x, $y, $r)) {
+            echo (
+            "<tr>
+                <td>" . "Неправильно введенные данные." . "</td>
+             </tr>");
+            return;
+        }
+
+        $result = checkHit($x, $y, $r);
+        $time = number_format((microtime(true) - $start) * 1000000, 2, ",", ".") . " мкс";
+
+        echo ("<tr>
+        <td>" . $x ."</td>
+        <td>" . $y . "</td>
+        <td>". $r ."</td>
+        <td>" . $result . "</td>
+        <td>" . $time . "</td>
+      </tr>");
+    }
+
+    function isValid($x, $y , $r) {
+        if (!is_double($x) || !is_double($y) || !is_double($r)) {
+            return false;
+        }
+
+        if ($x > 3 || $x < -5) {
+            return false;
+        }
+
+        if (!in_array($y, Y_VALUES)) {
+            return false;
+        }
+
+        if (!in_array($r, R_VALUES)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function checkHit($x, $y , $r) {
+        if (hitToCircle($x, $y, $r) || hitToRectangle($x, $y, $r) || hitToTriangle($x, $y, $r)) {
+            return "Попадание";
+        }
+        return "Не попадание";
+    }
+
+    function hitToRectangle($x, $y, $r) {
+        if ( (-$r <= $x) && ($x <= 0) && (0 <= $y) && ($y <= ($r/2) ) ) {
+            return true;
+        }
+        return false;
+    }
+
+    function hitToCircle($x, $y, $r) {
+        if ( (0 <= $x) && ($x <= sqrt($r^2 - $y^2)) && (0 <= $y) && ($y <= sqrt($r^2 - $x^2)) ) {
+            return true;
+        }
+        return false;
+    }
+
+    function hitToTriangle($x, $y, $r) {
+        if ( ((-$y - $r)/2 <= $x) && ($x <= 0) && (-2 * $x - $r <= $y) && ($y <= 0) ) {
+            return true;
+        }
+        return false;
+    }
 ?>
--- script.js
+++ script.js
@@ -1,57 +1,51 @@
 $(function () {
+
+    var timeNode = document.getElementById('time-node');
+
+    function getCurrentTimeString() {
+        return new Date().toTimeString().replace(/ .*/, '');
+    }
+
+    setInterval(
+        () => timeNode.innerHTML = getCurrentTimeString(),
+        1000
+    );
+
     $('#checkForm').submit(function (e) {
-        var coordinateX = $('#coordinateX').val().trim();
+
+        var coordinateX = $('#coordinateX').val();
         var coordinateY = $('input[name="coordinateY"]:checked').val();
 
-        if (coordinateX > 3 || coordinateX < -5 || coordinateX === '') {
+        $('.coordinateYE').next("div").remove();
+        $('#coordinateX').next("div").remove();
+
+        if (isNaN((coordinateX - 0)) || coordinateX > 3 || coordinateX < -5 || coordinateX === '') {
             e.preventDefault();
-            $('#coordinateX').next("div").remove();
-            $('#coordinateX').after('<div>This field is required</div>');
+            if (coordinateY === undefined) {
+                $('.coordinateYE').after('<div>Неверные данные</div>');
+            }
+            $('#coordinateX').after('<div>Неверные данные</div>');
+            return;
         }
 
         if (coordinateY === undefined) {
             e.preventDefault();
-            $('.coordinateYE').next("div").remove();
-            $('.coordinateYE').after('<div>This field is required</div>');
+            $('.coordinateYE').after('<div>Неверные данные</div>');
+            return;
         }
 
 
+        e.preventDefault();
+        $.ajax({
+            url: 'checkHitPoint.php',
+            type: "POST",
+            data: $('#checkForm').serialize(),
+            success: function (response) {
+                $(response).insertAfter($("tr:last"));
+            },
+            error: function(response) { // Данные не отправлены
+                alert("о оу");
+            }
+        });
     });
 });
-
-/*(function () {
-   //var el = document.getElementById("coordX");
-   var el = document.getElementById("checkForm");
-
-
-
-   el.addEventListener('submit', function (e){
-       e.preventDefault();
-       //var x = document.getElementById("coordX");
-       var elements = this.elements;
-       var x = elements.coordinateX.value;
-       document.getElementById('main').textContent = x;
-       alert(x);
-   });
-})
- */
-/*$(document).ready(function() {
-   $('#checkForm').submit(function (e) {
-       alert("ochko");
-       e.preventDefault();
-       var coordinateX = $('#coordX').val().trim();
-       var coordinateY = $("#coordinateY").val();
-
-
-       if (coordinateX > 3 || coordinateX < -5 || coordinateX === "") {
-           $('#coordX').after('<span class="error">This field is required</span>');
-       }
-   });
-});
-
- */
-
-
-
-
-
